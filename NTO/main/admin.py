c8499bc39411903e25c4289032a3e48ca620b548
user <e.kulpin@mail.ru>
2023-03-15 16:25:09 +0300

    site

--- NTO/main/admin.py
+++ NTO/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import ShareInfo
 # Register your models here.
+
+
+@admin.register(ShareInfo)
+class ShareInfoAdmin(admin.ModelAdmin):
+    fields = ('name', 'info')
--- NTO/main/models.py
+++ NTO/main/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+
+class ShareInfo(models.Model):
+    name = models.CharField(max_length=50)
+    info = models.JSONField()
+
+    def __str__(self) -> str:
+        return self.name
